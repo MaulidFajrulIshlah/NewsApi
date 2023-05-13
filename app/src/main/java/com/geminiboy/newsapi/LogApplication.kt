0bccca4dc17c71c26f6b86555153d95f929a7701
mauli <maulidfajrulishlah0306@gmail.com>
2023-05-13 17:31:34 +0700

    newsApi

---
+++ app/src/main/java/com/geminiboy/newsapi/LogApplication.kt
@@ -0,0 +1,11 @@
+package com.geminiboy.newsapi
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LogApplication : Application() {
+
+    
+
+}
---
+++ app/src/main/java/com/geminiboy/newsapi/model/articel/ResponseArticles.kt
@@ -0,0 +1,12 @@
+package com.geminiboy.newsapi.model.articel
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseArticles(
+    @SerializedName("articles")
+    val articles: List<Article>,
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("totalResults")
+    val totalResults: Int
+)
